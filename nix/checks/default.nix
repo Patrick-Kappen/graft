bb014cdf98c64d50eb2b23e1cad2c73e57db0cbb
Patrick Kappen <patrick@kappen.io>
2026-08-30 21:31:24 +0200

    fix(nix): cover incompatible worker API ranges

--- nix/checks/default.nix
+++ nix/checks/default.nix
@@ -316,6 +316,27 @@ let
   homeManagerHostIdMismatchRejected = lib.any (
     assertion: !assertion.assertion
   ) homeManagerMismatchEval.config.assertions;
+  incompatibleWorkerApiRangeEval = lib.evalModules {
+    specialArgs = { inherit pkgs; };
+    modules = [
+      moduleTestOptions
+      self.nixosModules.graft
+      {
+        services.graft = {
+          enable = true;
+          hostId = manifestHostId;
+          package = graftPackage // {
+            graftWorkerApiRange = graftWorkerApiRange // {
+              major = graftWorkerApiRange.major + 1;
+            };
+          };
+        };
+      }
+    ];
+  };
+  incompatibleWorkerApiRangeRejected = lib.any (
+    assertion: !assertion.assertion
+  ) incompatibleWorkerApiRangeEval.config.assertions;
 
   networkNixosEval = lib.evalModules {
     specialArgs = { inherit pkgs; };
@@ -949,6 +970,7 @@ in
       '';
 
   nixos-module-eval =
+    assert incompatibleWorkerApiRangeRejected;
     assert renderAssertions {
       rendered = nixosRendered;
       plainRendered = nixosPlainRendered;
